9cb53a25f6dc1f267098f31ddc883b19b981f707
Halil-Oksuz <halilcanoksuz66@gmail.com>
2024-08-20 21:55:25 +0300

    make header content interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,6 +4,7 @@ import formatCurrency from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
 import { renderPaymentSummary } from "./paymentSummary.js";
+import { renderCheckoutHeader } from "./checkoutHeader.js";
 
 export function renderOrderSummary() {
     let cartSummaryHTML = '';
@@ -63,33 +64,33 @@ export function renderOrderSummary() {
             const priceString = deliveryOption.priceCents === 0 ? 'FREE' : `$${formatCurrency(deliveryOption.priceCents)} -`;
             const isChecked = deliveryOption.id == cartItem.deliveryOptionId;
             html += `
-      <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
-        <input
-          type="radio"
-          ${isChecked ? 'checked' : ''}
-          class="delivery-option-input"
-          name="delivery-option-${matchingProduct.id}"
-        />
-        <div>
-          <div class="delivery-option-date">${dateString}</div>
-          <div class="delivery-option-price">${priceString} Shipping</div>
-        </div>
-      </div>
-    `
+            <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
+                <input
+                type="radio"
+                ${isChecked ? 'checked' : ''}
+                class="delivery-option-input"
+                name="delivery-option-${matchingProduct.id}"
+                />
+                <div>
+                <div class="delivery-option-date">${dateString}</div>
+                <div class="delivery-option-price">${priceString} Shipping</div>
+                </div>
+            </div>
+            `
         })
 
         return html;
     }
 
     document.querySelector('.order-summary').innerHTML = cartSummaryHTML;
-    updateCartQuantity();
+    renderPaymentSummary();
 
     document.querySelectorAll('.js-delete-link').forEach((link) => {
         link.addEventListener('click', () => {
             const { productId } = link.dataset;
             removeFromCart(productId);
-            const container = document.querySelector(`.js-cart-item-container-${productId}`);
-            container.remove();
+            renderCheckoutHeader();
+            renderOrderSummary();
             renderPaymentSummary();
         });
     });
@@ -108,6 +109,7 @@ export function renderOrderSummary() {
             const { productId } = link.dataset;
             const container = document.querySelector(`.js-cart-item-container-${productId}`);
             container.classList.add('is-editing-quantity');
+
         });
     });
 
@@ -145,18 +147,10 @@ export function renderOrderSummary() {
         }
         else {
             updateQuantity(productId, quantity);
-            updateCartQuantity();
-            updateQuantityLabel(productId);
+            renderCheckoutHeader();
+            renderOrderSummary();
+            renderPaymentSummary();
         }
-    }
-    function updateCartQuantity() {
-        let cartQuantity = calculateCartQuantity();
-        document.querySelector('.js-return-to-home-link').innerHTML = `${cartQuantity} items`;
-    }
-
-    function updateQuantityLabel(productId) {
-        const findingProduct = cart.find((cartItem) => cartItem.id === productId);
-        document.querySelector(`.quantity-label-${productId}`).innerHTML = findingProduct.quantity;
     }
 }
 
